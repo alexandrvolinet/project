11dd58ffdb7724c1a2995b65d034a5268fa26182
alexandrvolinet <alexandr.volinet@gmail.com>
2021-09-21 22:48:06 +0300

    function has been added: when you click again on the active accordion element, it closes

--- js/accordion.js
+++ js/accordion.js
@@ -1,21 +1,17 @@
 const lists = document.querySelectorAll(".feature-sub")
 const btns = document.querySelectorAll(".feature__link")
 
-
 btns.forEach((btnItem, index) => {
     btnItem.addEventListener('click', () => {
-        btns.forEach((btnItem) => {
-            btnItem.classList.remove("feature__link_active")
+        btns.forEach((btn, idx) => {
+            if (btn === btnItem) {
+                lists[idx].classList.toggle("hidden");
+                btn.classList.toggle("feature__link_active")
+            }
+            else {
+                lists[idx].classList.add("hidden")
+                btn.classList.remove("feature__link_active")
+            }
         })
-
-        btnItem.classList.add("feature__link_active")
-
-        lists.forEach((listItem) => {
-            listItem.classList.add("hidden")
-        })
-
-        lists[index].classList.remove("hidden")
-
-        console.dir(btnItem);
     })
 })
